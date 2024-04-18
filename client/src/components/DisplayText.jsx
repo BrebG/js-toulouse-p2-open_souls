4809cc64c44e51b24d8b074e44e5fd4233738e88
JeromeBslz <jerome933@outlook.com>
2024-04-18 17:18:00 +0200

    fix: add-prop-types

--- client/src/components/DisplayText.jsx
+++ client/src/components/DisplayText.jsx
@@ -1,11 +1,21 @@
+import PropTypes from "prop-types";
 
 function DisplayText({ text }) {
     return (
         <div>
             <p>{text.content}</p>
-            <h1>{text.title}</h1>
         </div>
     );
 }
+
+DisplayText.propTypes = {
+
+    text: PropTypes.shape({
+  
+      content: PropTypes.string.isRequired,
+        
+    }).isRequired,
+  
+  }
 
 export default DisplayText;

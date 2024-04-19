54813e4cd59b223e7a428cfc9045c044163ffacf
Fedal-leonard <leonardfedal@gmail.com>
2024-04-19 11:54:41 +0200

    fix: css and route of some pages

--- client/src/pages/ChoicePage.jsx
+++ client/src/pages/ChoicePage.jsx
@@ -4,13 +4,16 @@ import "./choicepage.scss";
 function ChoiceButtons() {
   return (
     <div className="choicepage">
-      <h2 id="choicetitle">We are here</h2>
-      <p id="choicetext">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius, quaerat
-        quidem! Quia, ipsam. Porro sint optio, nostrum ipsa temporibus aliquid
-        quod nam! Quae voluptatem saepe quas voluptas illo culpa non.
-      </p>
-
+      <div className="storytext">
+        <div className="loretext">
+          <p id="choicetext">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius,
+            quaerat quidem! Quia, ipsam. Porro sint optio, nostrum ipsa
+            temporibus aliquid quod nam! Quae voluptatem saepe quas voluptas
+            illo culpa non.
+          </p>
+        </div>
+      </div>
       <h3>Make your choice</h3>
       <div className="choicebutton">
         <Link to="/loosing-page" className="link-button">

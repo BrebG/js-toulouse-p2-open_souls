474027f4c8b45a5290437b97628af365183ab817
superwann32 <142965165+superwann32@users.noreply.github.com>
2024-04-24 11:22:54 +0200

    fix: pre pull

---
+++ client/src/tools/ItemArray.jsx
@@ -0,0 +1,54 @@
+// import {useState, useEffect} from 'react'
+
+// const [weapon, setWeapon] = useState(0);
+// const [shield, setShield] = useState(0);
+// function name(params) {
+
+// }
+// useEffect(() => {
+// fetch("https://eldenring.fanapis.com/api/weapons?limit=100&page=0")
+//     .then((response) => response.json())
+//     .then((data) => setWeapon(data.id))
+//     .catch((err) => {
+//         console.error(err);
+//     });
+// }, []);
+
+// useEffect(() => {
+//     fetch("https://eldenring.fanapis.com/api/weapons?limit=100&page=0")
+//     .then((response) => response.json())
+//     .then((data) => setShield(data.id))
+//     .catch((err) => {
+//         console.error(err);
+//     });
+// }, []);
+
+// const loreItems = [
+//     {
+//         id:"17f69728e6al0i1peyh4scwxfhl357",
+//         name:"Nagakiba",
+//         dex: 2
+//     },
+//     {
+//         id:"w2",
+//         name: "epee droite usee",
+//         strength: 1
+
+//     },
+//     {
+//         id:"w3",
+//         name:"bouclier meduse",
+//         strength:1,
+//         defense:1
+//     },
+//     {
+//         id:"w4",
+//         name:"bouclier en bois entaché",
+//         defense:1
+//     },
+//     {
+//         id:"17f69d42419l0i1oujp0m1csaaqdanf",
+//         name:"reduvia",
+//         dex:3
+//     }
+// ]

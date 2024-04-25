9b421488e6cd9017b103c8e9a5a2ece02434027e
BrebG <brebelgael@protonmail.com>
2024-04-25 15:09:05 +0200

    feat: cooldown on attack

--- client/src/components/CombatAction.jsx
+++ client/src/components/CombatAction.jsx
@@ -1,29 +1,44 @@
 import PropTypes from "prop-types";
-// import { useStats } from "../contexts/StatsContext";
+import { useState } from "react";
+import { useStats } from "../contexts/StatsContext";
 
 function CombatAction({ dealDamage, receiveDamage }) {
-  // const { classe } = useStats()
+  const [isButtonDisabled, setButtonDisabled] = useState(false);
+
+  const disableButton = () => {
+    setButtonDisabled(true);
+    setTimeout(() => {
+      setButtonDisabled(false);
+    }, 2000);
+  };
+
+  const { chosenClass } = useStats();
+
   return (
     <div className="CombatAction">
       <button
         type="button"
+        disabled={isButtonDisabled}
         onClick={() => {
-          dealDamage(10);
+          dealDamage(10 * (chosenClass.stats.strength / 10));
           receiveDamage(10);
+          disableButton();
         }}
       >
         Light attack
       </button>
       <button
         type="button"
+        disabled={isButtonDisabled}
         onClick={() => {
-          dealDamage(20);
+          dealDamage(20 * (chosenClass.stats.strength / 10));
           receiveDamage(10);
         }}
       >
         Heavy attack
       </button>
     </div>
+
   );
 }
 

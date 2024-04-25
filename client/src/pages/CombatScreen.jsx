0a27f06df37f3ad7282531245a831dd3c4c25d0d
JeromeBslz <jerome933@outlook.com>
2024-04-25 11:14:11 +0200

    feat: create-combat-screen-scss

--- client/src/pages/CombatScreen.jsx
+++ client/src/pages/CombatScreen.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import "./CombatScreen.scss";
 import Ennemy from "../components/Ennemy";
 import Player from "../components/Player";
 import CombatAction from "../components/CombatAction";
